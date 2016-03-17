fc1cdaf3c21579100ad0320bf3bb4adf84e90fa0
russell gillespie <rlg532@dhcp-129-105-244-166.sesp.northwestern.edu>
2016-03-17 15:02:06 -0500

    fleshing out template test with media files

--- public/src/Boot.js
+++ public/src/Boot.js
@@ -46,7 +46,7 @@ BasicGame.Boot.prototype = {
 
         //  Here we load the assets required for our preloader (in this case a background and a loading bar)
         this.load.image('preloaderBackground', 'images/winscreen.png');
-        this.load.image('preloaderBar', 'comb.png');
+        this.load.image('preloaderBar', 'images/comb.png');
 
     },
 
